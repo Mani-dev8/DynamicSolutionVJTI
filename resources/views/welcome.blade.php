839dc318bb3f0f5e8383361b3a161d065b63173e
manish <manishsewamahto090802@gmail.com>
2023-02-25 11:54:07 +0530

    completed updated

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,8 +4,8 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    @vite('resources/css/app.css')
-    <!-- <script src="https://cdn.tailwindcss.com"></script> -->
+    <!-- @vite('resources/css/app.css') -->
+    <script src="https://cdn.tailwindcss.com"></script>
     <!-- FONT -->
 
     <!-- <link rel="stylesheet" href="appointment.css"> -->
