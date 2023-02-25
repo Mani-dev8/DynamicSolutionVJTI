dc77d9e77e82ee2017ce0283aaf03b68479f4c19
manish <manishsewamahto090802@gmail.com>
2023-02-25 13:24:27 +0530

    final dynamic solution

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -547,6 +547,25 @@
 
     <script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js"></script>
     <script>
+        var accessToken2 = getParameterByName("access_token");
+        if (accessToken2 != null) {
+            document.getElementById('ancher_id').remove();
+            document.getElementById('anchor_signin').innerHTML="";
+            document.getElementById('registration_btn').innerHTML = `<a href="account" class="inline-flex items-center justify-center h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-purple-600 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none" aria-label="Sign In" title="Sign In">
+                                Account
+                            </a>`;
+            document.getElementById('anchor_signin').innerHTML = `
+                        <a id='logout' href='signin' class="flex justify-start items-center space-x-6 hover:text-white focus:bg-gray-700 focus:text-white hover:bg-gray-700 text-gray-400 rounded px-3 py-2  w-full md:w-52">
+                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M17 11H7C5.89543 11 5 11.8955 5 13V19C5 20.1046 5.89543 21 7 21H17C18.1046 21 19 20.1046 19 19V13C19 11.8955 18.1046 11 17 11Z" stroke="#9CA3AF" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                            <path d="M12 17C12.5523 17 13 16.5523 13 16C13 15.4477 12.5523 15 12 15C11.4477 15 11 15.4477 11 16C11 16.5523 11.4477 17 12 17Z" stroke="#9CA3AF" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                            <path d="M8 11V7C8 5.93913 8.42143 4.92172 9.17157 4.17157C9.92172 3.42143 10.9391 3 12 3C13.0609 3 14.0783 3.42143 14.8284 4.17157C15.5786 4.92172 16 5.93913 16 7V11" stroke="#9CA3AF" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                        </svg>
+                        <p class="text-base leading-4 ">Log out</p>
+                    </a>
+                        `;
+
+        }
         console.log("session== " + "{{session('user')}}")
         if (window.location.href.includes("account") || window.location.href.includes("login") || window.location.href.includes("signup")) {
             document.getElementById('Main').style.display = "none";
